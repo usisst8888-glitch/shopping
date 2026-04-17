6393529d3851c5e7ea8488448168fb38021335cd
배철응 <baecheol-eung@baecheol-eung-ui-MacBookPro.local>
2026-04-17 19:24:11 +0900

    누락된 페이지에 로딩 인디케이터 추가
    - 로그인/회원가입 페이지 loading.tsx 추가
    - 상품 등록/수정 페이지 loading.tsx 추가

---
+++ app/admin/(dashboard)/products/[id]/edit/loading.tsx
@@ -0,0 +1,5 @@
+import { LoadingSpinner } from '@/components/ui/loading-spinner'
+
+export default function ProductEditLoading() {
+  return <LoadingSpinner message="상품 정보 로딩 중..." />
+}
---
+++ app/admin/(dashboard)/products/new/loading.tsx
@@ -0,0 +1,5 @@
+import { LoadingSpinner } from '@/components/ui/loading-spinner'
+
+export default function ProductNewLoading() {
+  return <LoadingSpinner message="상품 등록 페이지 로딩 중..." />
+}
